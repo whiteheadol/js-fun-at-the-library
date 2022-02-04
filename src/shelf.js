a2568ba2d77510ae9924be654058008a94715bda
Olivia Whitehead <oliviawhitehead3@gmail.com>
2022-02-04 11:42:23 -0700

    Add shelf.js

--- src/shelf.js
+++ src/shelf.js
@@ -1,5 +1,4 @@
 //Shelf book
-
 // var sciFiShelf = [];
 //Originally had the sciFiShelf array declared as well,
 //didn't need it though?
@@ -9,10 +8,45 @@ function shelfBook(book, sciFiShelf) {
     }
   };
 
+//unshelfBook
+function unshelfBook(bookTitle, sciFiShelf) {
+    for (var i = 0; i < sciFiShelf.length; i++) {
+      if (bookTitle === sciFiShelf[i].title) {
+        sciFiShelf.splice(i, 1);
+      }
+    }
+  };
+
+//listTitles
+// function listTitles(fantasyShelf) {
+//   for (var i = 0; i < fantasyShelf.length; i++) {
+//     console.log(fantasyShelf[i].title);
+//   }
+// };
+function listTitles(fantasyShelf) {
+  var titles = [];
+  for (var i = 0; i < fantasyShelf.length; i++) {
+    titles.push(fantasyShelf[i].title);
+  }
+  return titles.join(", ");
+};
+
+//searchShelf
+function searchShelf(shelf, bookTitle) {
+  for (var i = 0; i < shelf.length; i++) {
+    if (bookTitle === shelf[i].title) {
+      return true;
+    }
+  }
+  return false;
+};
+
+// var bookShelf = [book.genre + 'Shelf'];
+
 
 module.exports = {
   shelfBook,
-  // unshelfBook,
-  // listTitles,
-  // searchShelf
+  unshelfBook,
+  listTitles,
+  searchShelf
 };
